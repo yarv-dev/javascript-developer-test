cad1c2029fc3e00367a9c2eca5f9bb3a29163ab7
BandoftheHawk <timojrvnn113@gmail.com>
2022-02-07 22:54:09 +1100

    Solution

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,30 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+	// put promises into array
+	// keeps it DRY.
+	const promises = [];
+	urls.forEach((url) => {
+		promises.push(httpGet(url));
+	});
+
+	// wait till all the promises have resolved
+	const responses = await Promise.all(promises);
+
+	const results = [];
+	responses.forEach((response) => {
+		if (response.status == 200) {
+			// parse the request and place in object
+			// Normally i'd prefer to have proper interface and undefined checks here too
+			results.push({ "Arnie Quote": JSON.parse(response.body).message });
+		} else {
+			results.push({ FAILURE: JSON.parse(response.body).message });
+		}
+	});
+
+	return results;
 };
 
 module.exports = {
-  getArnieQuotes,
+	getArnieQuotes,
 };
